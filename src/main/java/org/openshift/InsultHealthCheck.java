1333d0d56b2c4ca759b003982c4c0f1f5fe7eb63
Andreas Bublak <Andreas.Bublak@msg-david.de>
2020-02-14 14:36:20 +0100

    healthcheck updated

--- src/main/java/org/openshift/InsultHealthCheck.java
+++ src/main/java/org/openshift/InsultHealthCheck.java
@@ -5,8 +5,12 @@ import javax.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+import org.eclipse.microprofile.health.Readiness;
 
 @Health
+@Readiness
+@Liveness
 @ApplicationScoped
 public class InsultHealthCheck implements HealthCheck {
     public HealthCheckResponse call() {
